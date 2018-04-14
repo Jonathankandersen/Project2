a8f5a7e1816396646ad5613edf12afc1ea87ef21
Jonathankandersen <36509453+Jonathankandersen@users.noreply.github.com>
2018-04-14 16:58:08 +0200

    3. Commit: - Restored previous code

--- src/automat/Billetautomat.java
+++ src/automat/Billetautomat.java
@@ -1,127 +1,174 @@
 package automat;
-/**
- * Model af en simpel billetautomat til enkeltbilletter med én fast pris.
- */
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalDateTime;
+import java.util.Scanner;
+
 public class Billetautomat {
-	private int billetpris;    // Prisen for én billet.
-	private int balance; // Hvor mange penge kunden p.t. har puttet i automaten
-	private int antalBilletterSolgt; // Antal billetter automaten i alt har solgt
-	private boolean montørtilstand;
 
-	/**
-	 * Opret en billetautomat der sælger billetter til 10 kr.
-	 */
-	public Billetautomat() {
-		billetpris = 10;
-		balance = 0;
-		antalBilletterSolgt = 0;
-	}
+    private int billetpris;    // Prisen for én billet.
+    private int balance; // Hvor mange penge kunden p.t. har puttet i automaten
+    private int antalBilletterSolgt; // Antal billetter automaten i alt har solgt
+    private int medlemspris;        // Prisen for én billet til medlemmer
+    private boolean montørtilstand;
+    private boolean medlemstilstand;
+    private int medlemsType;
 
-	/**
-	 * Giver prisen for en billet. 
-	 */
-	public int getBilletpris() {
-		int resultat = billetpris;
-		return resultat;
-	}
+    /**
+     * Opret en billetautomat der sælger billetter til 10 kr.
+     */
+    public Billetautomat() {
+        billetpris = 10;
+        balance = 0;
+        antalBilletterSolgt = 0;
+    }
 
-	/**
-	 * Modtag nogle penge (i kroner) fra en kunde.
-	 */
-	public void indsætPenge(int beløb) {
-		balance = balance + beløb;
-	}
+    /**
+     * Giver prisen for en billet.
+     */
+    public int getBilletpris() {
+        int resultat = billetpris;
+        return resultat;
+    }
 
-	/**
-	 * Giver balancen (beløbet maskinen har modtaget til den næste billet).
-	 */
-	public int getBalance() {
-		return balance;
-	}
+    /**
+     * Modtag nogle penge (i kroner) fra en kunde.
+     */
+    public void indsætPenge(int beløb) {
+        if (beløb > 0 && beløb <= 1000) { //Første Krav Ændring med Boolan Udtryk 
+            if (balance < 1000) { // Første krav sikre at der ikke kan indsættes mere end 1000 total
+                balance = balance + beløb;
+            } else {
+                System.err.println("Der kan max indsættes 1000 kr "); // Første krav vis over 1000 prøves at indsættes 
+            }
+        } else if (balance > 1000) {
+            System.err.println("Max balance er 1000 kroner");
+        } else if (beløb < 0) { // ændring 
+            System.err.println("Hvis du ønsker penge retur tast '3'");
+        } else if (beløb > 1000) { // Første Krav MAX 1000 KR kan indsættes 
+            System.err.println("Det maximale beløb der kan indsættes er 1000 KR");
+        } else {
+            System.err.println("Forkert indtastning");
+        }
+    }
 
-	/**
-	 * Udskriv en billet.
-	 * Opdater total og nedskriv balancen med billetprisen
-	 */
-	public void udskrivBillet() {
-		if (balance<10) {
-			System.out.println("Du mangler at indbetale nogle penge");
-		}
-		System.out.println("##########B##T#########");
-		System.out.println("# BlueJ Trafikselskab #");
-		System.out.println("#                     #");
-		System.out.println("#        Billet       #");
-		System.out.println("#        " + billetpris + " kr.       #");
-		System.out.println("#                     #");
-		System.out.println("##########B##T#########");
-		System.out.println("# Du har " + (balance-billetpris) + " kr til gode       #");
-		System.out.println("##########B##T#########");
-		System.out.println();
+    /**
+     * Giver balancen (beløbet maskinen har modtaget til den næste billet).
+     */
+    public int getBalance() {
+        return balance;
+    }
 
-		antalBilletterSolgt = antalBilletterSolgt + 1;
-		balance = balance - billetpris; // Billetter koster 10 kroner
-	}
+    /**
+     * Udskriv en billet. Opdater total og nedskriv balancen med billetprisen
+     */
+    public void udskrivBillet() {
+        if (balance < 10) {
+            System.out.println("Du mangler at indbetale nogle penge");
+        }
+        System.out.println("##########B##T#########");
+        System.out.println("# BlueJ Trafikselskab #");
+        System.out.println("#                     #");
+        System.out.println("#        Billet       #");
+        System.out.println("#        " + billetpris + " kr.       #");
+        System.out.println("#                     #");
+        System.out.println("##########B##T#########");
+        System.out.println("# Du har " + (balance - billetpris) + " kr til gode       #");
+        System.out.println("##########B##T#########");
+        System.out.println();
 
+        antalBilletterSolgt = antalBilletterSolgt + 1;
+        balance = balance - billetpris; // Billetter koster 10 kroner
+    }
 
-	public int returpenge() {
-		int returbeløb = balance;
-		balance = 0;
-		System.out.println("Du får "+returbeløb+" kr retur");
-		return returbeløb;
-	}
+    public int returpenge() {
+        int returbeløb = balance;
+        balance = 0;
+        System.out.println("Du får " + returbeløb + " kr retur");
+        return returbeløb;
+    }
 
-	
-	void montørLogin(String adgangskode) {
-		if ("1234".equals(adgangskode)) {
-			montørtilstand = true;
-			System.out.println("Montørtilstand aktiveret");
-			System.out.println("Du kan nu angive billetpris");
-		} else {
-			montørtilstand = false;
-			System.out.println("Montørtilstand deaktiveret");
-		}
-	}
+    void montørLogin(String adgangskode) {
+        if ("1234".equals(adgangskode)) {
+            montørtilstand = true;
+            System.out.println("Montørtilstand aktiveret");
+            System.out.println("Du kan nu angive billetpris");
+        } else {
+            montørtilstand = false;
+            System.out.println("Montørtilstand deaktiveret");
+        }
+    }
 
+    public int getTotal() {
+        if (montørtilstand) {
+            return billetpris * antalBilletterSolgt;
+        } else {
+            System.out.println("Afvist - log ind først");
+            return 0;
+        }
+    }
 
-	public int getTotal() {
-		if (montørtilstand) {
-			return billetpris * antalBilletterSolgt;
-		} else {
-			System.out.println("Afvist - log ind først");
-			return 0;
-		}
-	}
+    public int getAntalBilletterSolgt() {
+        if (montørtilstand) {
+            return antalBilletterSolgt;
+        } else {
+            System.out.println("Afvist - log ind først");
+            return 0;
+        }
+    }
 
-	public int getAntalBilletterSolgt() {
-		if (montørtilstand) {
-			return antalBilletterSolgt;
-		} else {
-			System.out.println("Afvist - log ind først");
-			return 0;
-		}
-	}
+    public void setBilletpris(int billetpris) {
+        this.billetpris = billetpris;
+    }
 
-	public void setBilletpris(int billetpris) {
-		this.billetpris = billetpris;
-	}
+    public void nulstil() {
+        if (montørtilstand) {
+            antalBilletterSolgt = 0;
+        } else {
+            System.out.println("Afvist - log ind først");
+        }
+    }
 
-	public void nulstil() {
-		if (montørtilstand) {
-			antalBilletterSolgt = 0;
-		} else {
-			System.out.println("Afvist - log ind først");
-		}
-	}
+    public void setAntalBilletterSolgt(int antalBilletterSolgt) {
+        if (montørtilstand) {
+            this.antalBilletterSolgt = antalBilletterSolgt;
+        } else {
+            System.out.println("Afvist - log ind først");
+        }
+    }
 
-	public void setAntalBilletterSolgt(int antalBilletterSolgt) {
-		if (montørtilstand) {
-			this.antalBilletterSolgt = antalBilletterSolgt;
-		} else {
-			System.out.println("Afvist - log ind først");
-		}
-	}
+    public boolean erMedlem() {
+        return medlemstilstand;
+    }
+    
+   
 
-	public boolean erMontør() {
-		return montørtilstand;
-	}
+    public void rabatBillet() {
+        if (medlemstilstand) {
+            switch (medlemsType) {
+                case 1:
+                    billetpris = 5;
+                    break;
+                case 2:
+                    billetpris = 10;
+                    break;
+                case 3:
+                    billetpris = 15;
+                    break;
+                case 4:
+                    billetpris = 20;
+                    break;
+            }
+            this.billetpris = medlemspris;
+            System.out.println("Din billetpris " + medlemspris);
+        } else {
+            System.out.println("Du betaler normalpris");
+        }
+    }
+
+    public boolean erMontør() {
+        return montørtilstand;
+    }
 }

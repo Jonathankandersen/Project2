be5c8bd1c6f5cc7ff720c40962d440d92e5073f2
Korfax124 <saxo.kjs@gmail.com>
2018-04-24 11:52:54 +0200

    Først tilføjelse af grafik
    Added: Billet-class, GrafiskBilletautomat-class and form
    Adjusted: Billetautomat

---
+++ src/automat/Billet.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package automat;
+
+/**
+ *
+ * @author saxok_000
+ */
+public class Billet {
+    private int billetpris;
+    private String billettype;
+    
+    public Billet(String type, int pris) {
+        billettype = type;
+        billetpris = pris;
+    }
+    
+    public int GetBilletpris() {
+        
+    }
+}
